a0f31d1fa09e9c3f103523b1826a46144dbdbbf2
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2021-09-12 04:45:29 +0600

    Other Pages Activation

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ const btn = document.querySelector("button.mobile-menu-button");
 				btn.addEventListener("click", () => {
 					menu.classList.toggle("hidden");
 				});
+
+
+let home = () => {
+	location.replace("index.html")
+}
+let  researchAndPublications = () => {
+	location.replace("research-and-publications.html")
+}
+let projects = () => {
+	location.replace("projects.html")
+}
+let resume = () => {
+	location.replace("resume.html")
+}
+let blog = () => {
+	location.replace("blog.html")
+}
+let contact = () => {
+	location.replace("contact.html")
+}
